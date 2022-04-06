da76eb3b41f41295cc57c5c8bc05bdd31d8e5ca5
Amy Mick <66137093+amym321@users.noreply.github.com>
2022-04-06 17:47:43 -0500

    Map popup

--- assets/popup.js
+++ assets/popup.js
@@ -1,29 +1,36 @@
 window.onload = function(){ 
 
-    var outerModal = document.getElementById("why-buy-popup");
-    var btn = document.getElementById("why-buy-btn");
-  
-    btn.onclick = function() {
-      outerModal.style.display = "flex";
-    }
-  
-    document.addEventListener(
-      "click",
-      function(event) {
-        // If user either clicks X button OR clicks outside the modal window, then close modal 
-        if (
-          event.target.matches(".close-why-popup") ||
-          !event.target.closest("#why-modal-inner") && 
-          event.target.matches("#why-outer-modal") 
-        ) {
-          closeModal()
-        }
-      },
-      false
-    )
-    
-    function closeModal() {
-      outerModal.style.display = "none";
-    }
-  
+  var outerModal = document.getElementById("why-buy-popup");
+  var btn = document.getElementById("why-buy-btn");
+  var mapModal = document.getElementById("map-popup");
+  var mapBtn = document.getElementById("map-btn");
+
+  btn.onclick = function() {
+    outerModal.style.display = "flex";
   }
+
+  mapBtn.onclick = function() {
+    mapModal.style.display = "flex";
+  }
+  
+  document.addEventListener(
+    "click",
+    function(event) {
+      // If user either clicks X button OR clicks outside the modal window, then close modal 
+      if (
+        event.target.matches(".close-why-popup") ||
+        !event.target.closest("#why-modal-inner") && 
+        event.target.matches("#why-outer-modal") 
+      ) {
+        closeModal()
+      }
+    },
+    false
+  )
+   
+  function closeModal() {
+    outerModal.style.display = "none";
+    mapModal.style.display = "none";
+  }
+
+}
